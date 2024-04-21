7c25042f6fc70db947aa767c33cad43b0fb6e678
HakimAbdul12 <abdulhakimaben@gmail.com>
2024-04-21 11:46:12 +0000

    routes case insensitive

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express"
 import bodyParser from "body-parser"
+import usersRoutes from "./routes/users.js"
 
 const app = express()
 const port = 3000
 
+// app.set('case sensitive routing', true);
 app.use(bodyParser.json())
 app.listen(port, () => console.log(`Listening http://localhost/${port}/`))
 app.get("/", (req, res) => res.send("Hello World!"))
+app.use('/users', usersRoutes);
