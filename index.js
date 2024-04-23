28d00c60bcd0541c9fdfd852af31a6c36492496c
HakimAbdul12 <abdulhakimaben@gmail.com>
2024-04-23 14:07:32 +0000

    socket integration

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-import express from "express"
-import bodyParser from "body-parser"
-import usersRoutes from "./routes/users.js"
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import bodyParser from "body-parser";
+import usersRoutes from "./routes/users.js";
 
-const app = express()
-const port = 3000
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 3000;
 
-// app.set('case sensitive routing', true);
-app.use(bodyParser.json())
-app.listen(port, () => console.log(`Listening http://localhost/${port}/`))
-app.get("/", (req, res) => res.send("Hello World!"))
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile('C:/My_Programs/Express/first-app-api/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('chatMessage', (message) => {
+    console.log('Received message:', message);
+    // Broadcast the message to all clients
+    io.emit('chatMessage', message);
+  });
+});
+
 app.use('/users', usersRoutes);
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
